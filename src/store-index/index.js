6b4e785284d8859abc972d5259cc65334f1acebc
renaud23 <renaud.genevois23@gmail.com>
2021-03-12 08:59:58 +0100

    init repo

---
+++ src/store-index/index.js
@@ -0,0 +1,1 @@
+export { default as StoreTools } from './store-tools';
---
+++ src/stories/create-store.stories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StoreTools } from '../store-index';
+
+export function Tools() {
+	return <StoreTools />;
+}
+
+const story = {
+	title: 'store-index/StoreTools',
+	component: StoreTools,
+};
+
+export default story;
