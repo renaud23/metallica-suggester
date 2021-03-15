04423cc22519790c39a4ec4512ad360c56215c7b
renaud23 <renaud.genevois23@gmail.com>
2021-03-15 11:08:04 +0100

    suggester story

---
+++ src/stories/suggester.stories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Suggester from '../suggester';
+
+export function DefaultSuggester() {
+	return <Suggester />;
+}
+
+const story = {
+	title: 'Suggester',
+	component: Suggester,
+};
+
+export default story;
