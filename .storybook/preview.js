8585cf554e78eaf5dd12ce0e759bb16f691b3253
renaud23 <renaud.genevois23@gmail.com>
2021-04-30 11:45:57 +0200

    worker path good

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,9 +2,10 @@ import { addDecorator } from '@storybook/react';
 import { initializeWorker, mswDecorator } from 'msw-storybook-addon';
 
 const BASE_PATH = process.env.BASE_PATH;
-console.log({ BASE_PATH });
 
-initializeWorker({ url: `${BASE_PATH}/mockServiceWorker.js` });
+initializeWorker({
+	serviceWorker: { url: `${BASE_PATH}/mockServiceWorker.js` },
+});
 addDecorator(mswDecorator);
 
 export const parameters = {
